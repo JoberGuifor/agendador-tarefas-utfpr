6c5f22f9831ff111d51f7c2aad2093fa6eeb0efb
Jober de Campos <jobergc@gmail.com>
2023-09-11 20:25:05 -0300

    atividade 8

--- agendador-tarefas-utfpr-angular/src/app/app.component.ts
+++ agendador-tarefas-utfpr-angular/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import * as M from 'materialize-css';
+
 import {  AfterViewInit,
   Component,
   ElementRef,
@@ -9,7 +11,7 @@ import {  AfterViewInit,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'agendador-tarefas-utfpr-angular';
   currentDate: Date;
 
@@ -21,5 +23,13 @@ export class AppComponent {
     setInterval(() => {
       this.currentDate = new Date();
     }, 1000);
+  }
+
+  ngAfterViewInit(): void {
+      let $sideNav = $('#mobile-demo');
+  }
+
+  onClick(){
+    alert("você disparou um event");
   }
 }
